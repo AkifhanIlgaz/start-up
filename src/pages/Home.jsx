a0c350efb4a98d9dcd2aa519b00fcb4f1c332873
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-18 11:44:58 +0300

    .

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,61 @@
-import { IonButton } from '@ionic/react'
-import { useEffect, useState } from 'react'
+import { IonAlert, IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonChip, IonIcon } from '@ionic/react'
+import { heartOutline } from 'ionicons/icons'
+import Cat from '../assets/cat.jpg'
+
+
+
+const Alert = () => {
+	return (
+		<div style={{ display: 'inline-block', float: 'right' }}>
+			<IonButton id="present-alert" color="danger" expand="size">
+				<IonIcon icon={heartOutline}></IonIcon>
+			</IonButton>
+
+			<IonAlert
+				header="Like"
+				trigger="present-alert"
+				buttons={[
+					{
+						text: 'Cancel',
+						role: 'cancel',
+						handler: () => {
+							setHandlerMessage('Alert canceled')
+						}
+					},
+					{
+						text: 'OK',
+						role: 'confirm',
+						handler: () => {
+							setHandlerMessage('Alert confirmed')
+						}
+					}
+				]}
+				onDidDismiss={({ detail }) => setRoleMessage(`Dismissed with role: ${detail.role}`)}
+			></IonAlert>
+		</div>
+	)
+}
 
 export const Home = () => {
-	const [data, setData] = useState(0)
-
-	useEffect(() => {
-		setData(100)
-	}, [])
-
 	return (
-		<div>
-			SAYAÇ: {data}
-			<IonButton onclick={() => setData(data + 1)}>Tıkla</IonButton>
-			<div>SAYAÇ: {data}</div>
-			<div>SAYAÇ: {data}</div>
-		</div>
+		<IonCard>
+			<img src={Cat} alt="Cat" width={'100%'} />
+			<IonCardHeader>
+				<IonCardTitle>
+					Pet Name
+					<Alert></Alert>
+				</IonCardTitle>
+			</IonCardHeader>
+
+			<IonCardContent>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit obcaecati repudiandae excepturi, vero nihil totam?</IonCardContent>
+			<div align="center" className="ion-padding-bottom">
+				<IonChip color="secondary">Cat</IonChip>
+				<IonChip color="secondary">Cat</IonChip>
+				<IonChip color="secondary">Cat</IonChip>
+				<IonChip color="secondary">Cat</IonChip>
+				<IonChip color="secondary">Cat</IonChip>
+			</div>
+		</IonCard>
 	)
 }
 
